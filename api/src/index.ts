1a58f828ee4f5bc51082ada78e704b38143a75a8
Paul Heising <paul.heising@wetteronline.de>
2020-03-05 18:08:27 +0100

    add a sub-route example

--- api/src/index.ts
+++ api/src/index.ts
@@ -5,7 +5,18 @@ const port = 8080; // default port to listen
 
 // define a route handler for the default home page
 app.get('/', (req, res) => {
-  res.send('Hello world!');
+  res.send({
+    temperature: 20,
+    rainHours: 3,
+  });
+});
+
+// route handler for a sub page
+app.get('/example-sub-route', (req, res) => {
+  res.send({
+    temperature: 15,
+    rainHours: 17,
+  });
 });
 
 // start the Express server
